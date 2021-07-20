c34dbcd3fe13a33e91e11f5ef65b8025f7b174f4
Ana Giancoli <paulagiancoli@gmail.com>
2021-07-20 16:40:54 -0300

    adiciona i18n

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
+import { I18n } from 'aws-amplify';
+import AmplifyI18n from 'amplify-i18n';
 import config from './aws-exports';
+
 Amplify.configure(config);
+
+const locales = ["en", "fr", "pt-BR"]
+AmplifyI18n.configure(locales)
+I18n.setLanguage("pt-BR")
 
 
 ReactDOM.render(
